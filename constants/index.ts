f2f1b2ff67505d7d98e0e9ff791e21e7d968e879
nesrine <nesrinekhbou7@gmail.com>
2024-08-30 20:37:42 +0100

    created 1st version with authentication

---
+++ constants/index.ts
@@ -0,0 +1,112 @@
+import arrowDown from "@/assets/icons/arrow-down.png";
+import arrowUp from "@/assets/icons/arrow-up.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import chat from "@/assets/icons/chat.png";
+import checkmark from "@/assets/icons/check.png";
+import close from "@/assets/icons/close.png";
+import dollar from "@/assets/icons/dollar.png";
+import email from "@/assets/icons/email.png";
+import eyecross from "@/assets/icons/eyecross.png";
+import google from "@/assets/icons/google.png";
+import home from "@/assets/icons/home.png";
+import list from "@/assets/icons/list.png";
+import lock from "@/assets/icons/lock.png";
+import map from "@/assets/icons/map.png";
+import marker from "@/assets/icons/marker.png";
+import out from "@/assets/icons/out.png";
+import person from "@/assets/icons/person.png";
+import pin from "@/assets/icons/pin.png";
+import point from "@/assets/icons/point.png";
+import profile from "@/assets/icons/profile.png";
+import search from "@/assets/icons/search.png";
+import selectedMarker from "@/assets/icons/selected-marker.png";
+import star from "@/assets/icons/star.png";
+import target from "@/assets/icons/target.png";
+import to from "@/assets/icons/to.png";
+import check from "@/assets/images/check.png";
+import getStarted from "@/assets/images/get-started.png";
+import message from "@/assets/images/message.png";
+import noResult from "@/assets/images/no-result.png";
+import onboarding1 from "@/assets/images/onboarding1.png";
+import onboarding2 from "@/assets/images/onboarding2.png";
+import onboarding3 from "@/assets/images/onboarding3.png";
+import signUpCar from "@/assets/images/signup-car.png";
+import appimage2 from "@/assets/images/appimage2.png";
+import appimage1 from "@/assets/images/appimage1.png";
+import appimage3_1 from "@/assets/images/appimage3_1.png";
+import appimage3_2 from "@/assets/images/appimage3_2.png";
+import appimage4 from "@/assets/images/appimage4.png";
+// eslint-disable-next-line import/no-unresolved
+import appimage5 from "@/assets/images/appimage5.png";
+export const images = {
+  onboarding1,
+  onboarding2,
+  onboarding3,
+  getStarted,
+  signUpCar,
+  check,
+  noResult,
+  message,
+  appimage2,
+  appimage1,
+  appimage3_1,
+  appimage3_2,
+  appimage4,
+  appimage5,
+};
+
+export const icons = {
+  arrowDown,
+  arrowUp,
+  backArrow,
+  chat,
+  checkmark,
+  close,
+  dollar,
+  email,
+  eyecross,
+  google,
+  home,
+  list,
+  lock,
+  map,
+  marker,
+  out,
+  person,
+  pin,
+  point,
+  profile,
+  search,
+  selectedMarker,
+  star,
+  target,
+  to,
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "SafeStep: Your Safe Journey Starts Here",
+    description:
+      "Experience enhanced mobility with real-time obstacle detection and fall prevention",
+    image: images.appimage2,
+  },
+  {
+    id: 2,
+    title: "See Clearly, Walk Safely",
+    description:
+      "Activate light in low light conditions and navigate your path with confidence",
+    image: images.appimage1,
+  },
+  {
+    id: 3,
+    title: "SafeStep: Confidence in Every Step",
+    description:
+      "Empower your mobility with advanced safety features and real-time alerts",
+    image: images.appimage3_2,
+  },
+];
+
+export const data = {
+  onboarding,
+};
